02fa54f2e2c55de03d50108859d571a18f8dcccb
Henry <jtad007@gmail.com>
2022-09-20 13:44:41 +0300

    added clickable buttons in Js

--- js/main.js
+++ js/main.js
@@ -4,5 +4,25 @@ const featuresDropdown = document.querySelector(".features-dropdown")
 let featuresBoxstate = false;
 featuresLinks.addEventListener("click", function(){
    featuresBoxstate = !featuresBoxstate;
-   console.log(featuresBoxstate) 
+//    console.log(featuresBoxstate)
+
+    if (featuresBoxstate === true){
+        featuresDropdown.style.display = "flex";
+    } else{
+        featuresDropdown.style.display = "none";
+    }
+})
+
+const companyLinks = document.querySelector(".company-link");
+const companyDropdown = document.querySelector(".company-dropdown");
+
+let companyBoxstate = false;
+companyLinks.addEventListener("click", function(){
+    companyBoxstate = !companyBoxstate;
+
+    if(companyBoxstate === true){
+        companyDropdown.style.display = "flex";
+    } else{
+        companyDropdown.style.display = "none";
+    }
 })
